3143cfef1a2c56720acd45994ab9c458f870f564
Рафаэль <106482482+Aris-turtle@users.noreply.github.com>
2024-03-24 22:29:51 +0300

    Update Test.java

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        UDRGenerator udrGen = new UDRGenerator();
-        udrGen.generateReport("70097280301");
+        
     }
 }
