6232954591663b5c8d9befcad8d417a0ab33744d
Rafael Akhmedov <ahmedovrafael999@gmail.com>
2024-03-24 18:05:08 +0300

    Add console output in first method

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -9,33 +9,7 @@ import java.util.Set;
 
 public class Test {
     public static void main(String[] args) {
-        Random random = new Random();
-//        UDRGenerator udrFile = new UDRGenerator("78516897504", Map.of("totalTime", "02:12:05"), Map.of("totalTime", "00:34:27"));
-//
-//        JSONObject jsonObject = new JSONObject();
-//        jsonObject.put("msisdn", udrFile.getMsisdn());
-//        jsonObject.put("incomingCall", udrFile.getIncomingCall());
-//        jsonObject.put("outcomingCall", udrFile.getOutcomingCall());
-//
-//        try {
-//            File file = new File("reports/test.json");
-//            file.createNewFile();
-//            FileWriter fileWriter = new FileWriter(file);
-//
-//            fileWriter.write(jsonObject.toJSONString());
-//            fileWriter.close();
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-        UDRGenerator udrGenerator = new UDRGenerator();
-        udrGenerator.generateReport();
-
-        //вывод суммарных минут на каждого пользователя
-        long totalCall = 0;
-        for (Map.Entry<String, Map<String, AdvancedLong>> m: udrGenerator.getSubscribersSummary().entrySet()){
-            String msisdn = m.getKey();
-            totalCall = m.getValue().get("01").getValue() + m.getValue().get("02").getValue();      //суммарное время разговоров
-            System.out.println(msisdn + " " + totalCall);
-        }
+        UDRGenerator udrGen = new UDRGenerator();
+        udrGen.generateReport();
     }
 }
