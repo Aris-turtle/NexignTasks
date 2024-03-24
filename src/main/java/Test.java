22225717713b7ef6e864f48413df8b5d62351fc3
Rafael Akhmedov <ahmedovrafael999@gmail.com>
2024-03-24 19:19:11 +0300

    Add console output in all methods

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -10,6 +10,6 @@ import java.util.Set;
 public class Test {
     public static void main(String[] args) {
         UDRGenerator udrGen = new UDRGenerator();
-        udrGen.generateReport();
+        udrGen.generateReport("74685487666", 5);
     }
 }
