7708ffb994e097a0050eaac24fa074e02e9eb133
Rafael Akhmedov <ahmedovrafael999@gmail.com>
2024-03-24 20:21:22 +0300

    Add json generator

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,12 +1,3 @@
-import org.json.simple.JSONObject;
-
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Map;
-import java.util.Random;
-import java.util.Set;
-
 public class Test {
     public static void main(String[] args) {
         UDRGenerator udrGen = new UDRGenerator();
