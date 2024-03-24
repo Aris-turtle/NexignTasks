a788ed3c605ae6bfc727b88b4f5ee21ccd9d2873
Рафаэль <106482482+Aris-turtle@users.noreply.github.com>
2024-03-24 22:31:00 +0300

    Update Application.java

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,11 +2,13 @@ import java.io.IOException;
 
 public class Application {
     public static void main(String[] args) throws IOException {
-        Subscribers subscribers = new Subscribers();
+        Subscribers subscribers = new Subscribers();    //генерация абонентов
         CDR cdrFile = new CDR();
 
-        cdrFile.generateCDRFile(subscribers.getNumbers());
+        cdrFile.generateCDRFile(subscribers.getNumbers());    //генерация cdr файлов
         System.out.println("CDR файлы успешно сгенерированы");
-        System.out.println(cdrFile.getCallLog());
+
+        UDRGenerator udrGen = new UDRGenerator();
+        udrGen.generateReport();    //генерация udr отчета
     }
 }
